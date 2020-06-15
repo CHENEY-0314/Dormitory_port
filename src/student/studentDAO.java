84fcdfcc6dc60bca02b15667119fa3be26675bf9
CHENEY-0314 <529269793@qq.com>
2020-06-15 11:29:56 +0800

    增加了一个GetStudentData的接口，用于获取学生相关数据

--- src/student/studentDAO.java
+++ src/student/studentDAO.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import net.sf.json.JSONObject;
 import DBManagerr.DBManager;
 
 
@@ -21,7 +24,7 @@ public class studentDAO {
 	 
 			//SQL查询语句
 			StringBuilder sqlStatement = new StringBuilder();
-			sqlStatement.append("select * from StudentLogin where ID=?");        //问号？的地方会被学号替换
+			sqlStatement.append("select * from StudentLogin where s_id=?");        //问号？的地方会被学号替换
 	    	
 			//设置数据库的字段值
 			try {
@@ -32,8 +35,8 @@ public class studentDAO {
 				
 				student user = new student();
 				if (resultSet.next()) {                                        //生成student对象并返回
-					user.setStudentNumber(resultSet.getString("ID"));           //设置账户、密码，根据返回的内容还可以设置其他信息
-					user.setPassword(resultSet.getString("PASSWORD"));
+					user.setStudentNumber(resultSet.getString("s_id"));           //设置账户、密码，根据返回的内容还可以设置其他信息
+					user.setPassword(resultSet.getString("password"));
 					return user;
 					} else {
 						return null;
@@ -46,6 +49,43 @@ public class studentDAO {
 						}
 			}
 		
-		
+	//根据学号查询学生的姓名、性别、宿舍（楼号、宿舍号、床位号），返回json格式。
+		public static JSONObject GetStudentByStuId(String id) {
+			
+			//SQL查询语句
+			StringBuilder sql1 = new StringBuilder();
+			sql1.append("select * from Student t1 inner join LiveRecord t2 on t1.s_id=t2.s_id where t1.s_id=?");        //问号？的地方会被学号id替换
+			
+			Connection connection = DBManager.getConnection();
+			PreparedStatement preparedStatement = null;
+
+	        Map<String, String> message = new HashMap<>();
+	        JSONObject jsonObject = new JSONObject();
+	        try {
+				preparedStatement = connection.prepareStatement(sql1.toString());
+				preparedStatement.setString(1, id);
+	        	} catch (SQLException ex) {
+	        		Logger.getLogger(studentDAO.class.getName()).log(Level.SEVERE, null, ex);
+	        		}
+	        ResultSet resultSet = null;
+	        try {
+	        	resultSet = preparedStatement.executeQuery();
+	        	if(resultSet.next()) {
+	        		message.put("s_id", resultSet.getString("s_id"));
+	        		message.put("s_name", resultSet.getString("s_name"));
+	        		message.put("sex", resultSet.getString("sex"));
+	        		message.put("building", resultSet.getString("building"));
+	        		message.put("room_num", resultSet.getString("room_num"));
+	        		message.put("bed_num", resultSet.getString("bed_num"));
+	        		jsonObject.put("结果", message);
+	        		}
+	        	} catch (SQLException ex) {
+		Logger.getLogger(studentDAO.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	        
+	        DBManager.closeAll(connection, preparedStatement, resultSet);
+	        return jsonObject;
+	        
+	}
 	
 }
